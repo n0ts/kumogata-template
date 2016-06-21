1d0e926d7c2b4e3036c0c689d227308cdba56ef0
Naoya Nakazawa <me@n0ts.org>
2016-06-21 16:29:50 +0900

    Delete unusing code

--- template/output-s3.rb
+++ template/output-s3.rb
@@ -4,5 +4,5 @@
 
 bucket = "#{args[:name]} bucket"
 
-_output "#{bucket} s3 domain name", ref_value: [ bucket, "DomainName" ] if args.key? :domain
+_output "#{bucket} s3 domain name", ref_value: [ bucket, "DomainName" ]
 _output "#{bucket} s3 web site url", ref_value: [ bucket, "WebsiteURL" ]
