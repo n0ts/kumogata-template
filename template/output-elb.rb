0c97df19db04e2c66762eb7a0402026c3b39c0b9
Naoya Nakazawa <me@n0ts.org>
2016-08-29 15:28:57 +0300

    Add ELB output parameter

--- template/output-elb.rb
+++ template/output-elb.rb
@@ -4,6 +4,10 @@
 
 _output "#{args[:name]} load balancer",
         ref_value: "#{args[:name]} load balancer"
+_output "#{args[:name]} load balancer canonical hosted zone name",
+        ref_value: [ "#{args[:name]} load balancer", "CanonicalHostedZoneName" ] if args.key? :route53
+_output "#{args[:name]} load balancer canonical hosted zone id",
+        ref_value: [ "#{args[:name]} load balancer", "CanonicalHostedZoneID" ] if args.key? :route53
 _output "#{args[:name]} load balancer dns name",
         ref_value: [ "#{args[:name]} load balancer", "DNSName" ]
 _output "#{args[:name]} load balancer security group name",
--- test/template/output-elb_test.rb
+++ test/template/output-elb_test.rb
@@ -44,5 +44,66 @@ _output_elb "test"
 }
     EOS
     assert_equal exp_template.chomp, act_template
+
+    template = <<-EOS
+_output_elb "test", route53: true
+    EOS
+    act_template = run_client_as_json(template)
+    exp_template = <<-EOS
+{
+  "TestLoadBalancer": {
+    "Description": "description of TestLoadBalancer",
+    "Value": {
+      "Ref": "TestLoadBalancer"
+    }
+  },
+  "TestLoadBalancerCanonicalHostedZoneName": {
+    "Description": "description of TestLoadBalancerCanonicalHostedZoneName",
+    "Value": {
+      "Fn::GetAtt": [
+        "TestLoadBalancer",
+        "CanonicalHostedZoneName"
+      ]
+    }
+  },
+  "TestLoadBalancerCanonicalHostedZoneId": {
+    "Description": "description of TestLoadBalancerCanonicalHostedZoneId",
+    "Value": {
+      "Fn::GetAtt": [
+        "TestLoadBalancer",
+        "CanonicalHostedZoneID"
+      ]
+    }
+  },
+  "TestLoadBalancerDnsName": {
+    "Description": "description of TestLoadBalancerDnsName",
+    "Value": {
+      "Fn::GetAtt": [
+        "TestLoadBalancer",
+        "DNSName"
+      ]
+    }
+  },
+  "TestLoadBalancerSecurityGroupName": {
+    "Description": "description of TestLoadBalancerSecurityGroupName",
+    "Value": {
+      "Fn::GetAtt": [
+        "TestLoadBalancer",
+        "SourceSecurityGroup.GroupName"
+      ]
+    }
+  },
+  "TestLoadBalancerSecuriryGroupOwner": {
+    "Description": "description of TestLoadBalancerSecuriryGroupOwner",
+    "Value": {
+      "Fn::GetAtt": [
+        "TestLoadBalancer",
+        "SourceSecurityGroup.OwnerAlias"
+      ]
+    }
+  }
+}
+    EOS
+    assert_equal exp_template.chomp, act_template
   end
 end
