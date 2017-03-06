05b0bbffc9683d7a9ab9ba983d142222fc95f5d8
Naoya Nakazawa <naoya.n@gmail.com>
2017-03-06 13:06:10 +0900

    Fix mappings ec2 test

--- test/template/mappings-ec2_test.rb
+++ test/template/mappings-ec2_test.rb
@@ -3,47 +3,7 @@ require 'kumogata/template/const'
 
 class MappingsEc2Test < Minitest::Test
   def test_normal
-    # from mappings-ec2
-    amazon_image_id = {
-      virginia: "08111162",
-      oregon: "c229c0a2",
-      california: "1b0f7d7b",
-      frankfurt: "e2df388d",
-      ireland: "31328842",
-      singapore: "e90dc68a",
-      sydney: "f2210191",
-      tokyo: "f80e0596",
-      seoul: "6598510b",
-      saopaulo: "1e159872",
-    }
-    amazon_images = get_image_values(amazon_image_id)
-    centos7_image_id = {
-      virginia: "6d1c2007",
-      oregon: "d2c924b2",
-      california: "af4333cf",
-      ireland: "7abd0209",
-      frankfurt: "9bf712f4",
-      singapore: "f068a193",
-      tokyo: "eec1c380",
-      sydney: "fedafc9d",
-      seoul: "c74789a9",
-      saopaulo: "26b93b4a",
-    }
-    centos7_images = get_image_values(centos7_image_id)
-    ubuntu16_image_id = {
-      virginia: "2ef48339",
-      oregon:	"746aba14",
-      california: "a9a8e4c9",
-      frankfurt: "a9a557c6",
-      ireland: "643d4217",
-      singapore: "42934921",
-      sydney: "623c0d01",
-      tokyo: "919cd68",
-      seoul: "", ## N/A
-      saopaulo: "60bd2d0c",
-    }
-    ubuntu16_images = get_image_values(ubuntu16_image_id)
-
+    images = generate_image_values
     archs = ""
     EC2_INSTANCE_TYPES.each_with_index do |type, i|
       archs += <<-EOS
@@ -56,20 +16,21 @@ class MappingsEc2Test < Minitest::Test
     template = <<-EOS
 _mappings_ec2 "test"
     EOS
-    act_template = run_client_as_json(template)
+    act_template = convert_image_values(run_client_as_json(template))
+
     exp_template = <<-EOS
 {
   "AWSInstanceType2Arch": {
 #{archs.chomp}
   },
   "AWSRegionArch2AMIAmazonLinuxOfficial": {
-#{amazon_images.chomp}
+#{images.chomp}
   },
   "AWSRegionArch2AMICentos7Official": {
-#{centos7_images.chomp}
+#{images.chomp}
   },
   "AWSRegionArch2AMIUbuntu16Official": {
-#{ubuntu16_images.chomp}
+#{images.chomp}
   }
 }
     EOS
@@ -77,16 +38,35 @@ _mappings_ec2 "test"
   end
 
   private
-  def get_image_values image_ids
+  def generate_image_values
     values = ''
     AWS_REGION.each_with_index do |v, i|
       region, location = v
       values += <<-EOS
     "#{location}": {
-      "HVM64": "ami-#{image_ids[region]}"
+      "HVM64": "ami-*"
     }#{i == (AWS_REGION.size - 1) ? '' : ','}
     EOS
     end
     values
+  end
+
+  def convert_image_values(values)
+    result = {}
+    JSON.parse(values).each do |k ,v|
+      if k == "AWSInstanceType2Arch"
+        result[k] = v
+        next
+      else
+        result[k] = {}
+      end
+      v.each do |kk, vv|
+        result[k][kk] = {}
+        vv.each do |kkk, vvv|
+          result[k][kk][kkk] = vvv.gsub(/^ami-(.*)/, 'ami-*')
+        end
+      end
+    end
+    JSON.pretty_generate(result)
   end
 end
