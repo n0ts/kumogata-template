91824275a838da37c0177e623714d96ad9e4374b
Naoya Nakazawa <naoya.n@gmail.com>
2017-03-21 17:55:48 +0900

    Change public_key output_ec2 resource

--- template/output-ec2.rb
+++ template/output-ec2.rb
@@ -2,7 +2,9 @@
 # Output ec2
 #
 
+public_ip = args[:public_ip] || false
+
 _output "#{args[:name]} instance", ref_value: "#{args[:name]} instance"
 _output "#{args[:name]} instance az", ref_value: [ "#{args[:name]} instance", "AvailabilityZone" ]
-_output "#{args[:name]} instance public ip", ref_value: [ "#{args[:name]} instance", "PublicIp" ] if args.key? :public_ip
+_output "#{args[:name]} instance public ip", ref_value: [ "#{args[:name]} instance", "PublicIp" ] if public_ip
 _output "#{args[:name]} instance private ip", ref_value: [ "#{args[:name]} instance", "PrivateIp" ]
