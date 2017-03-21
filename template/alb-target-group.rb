fbf1f1c4b38a224ede0245a1f7622f0adeb325f9
Naoya Nakazawa <naoya.n@gmail.com>
2017-03-21 19:44:53 +0900

    Add TargetGroupFullName property alb_target_group resource

--- template/alb-target-group.rb
+++ template/alb-target-group.rb
@@ -11,8 +11,14 @@ matcher = _alb_matcher(args)
 target_name = _ref_name("target_name", args)
 target_name = args[:name] if target_name.empty?
 attributes = _alb_attributes(args)
-port = args[:port] || 80
-protocol = _valid_values("protocol", %w( http https ), "http")
+full_name = _ref_string("full_name", args)
+protocol = _valid_values(args[:protocol], %w( http https ), "http")
+port =
+  if PORT.key? protocol.to_sym
+    PORT[protocol.to_sym]
+  else
+    args[:port] || PORT[:http]
+  end
 tags = _tags(args)
 targets = _alb_targets(args)
 vpc = _ref_string("vpc", args, "vpc")
@@ -31,8 +37,9 @@ _(name) do
     Port port
     Protocol protocol.upcase
     Tags tags
-    TargetGroupAttributes attributes
-    Targets targets
+    TargetGroupAttributes attributes unless attributes.empty?
+    TargetGroupFullName full_name unless full_name.empty?
+    Targets targets unless targets.empty?
     UnhealthyThresholdCount health_check[:unhealthly]
     VpcId vpc
   end
