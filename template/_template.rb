f74561edbfa1a75995684218b5636f7e8a37ef7d
n0ts <naoya.n@gmail.com>
2017-05-20 15:32:19 +0900

    Update base template

--- template/_template.rb
+++ template/_template.rb
@@ -1,25 +1,42 @@
+require 'aws-sdk'
+
 AWSTemplateFormatVersion "2010-09-09"
 
 Description (<<-EOS).undent
   Kumogata Template - #{NAME} stack
 EOS
 
+Metadata do
+end
+
 Parameters do
+  _parameter "stack name", default: "",
+                           description: "the name of this stack"
   _parameter "name", default: "#{NAME}",
                      description: "name of this stack"
   _parameter "service", default: "service",
                         description: "#{NAME} service"
-  _parameter "version", default: "1.0.0",
+  _parameter "version", default: "0.0.1",
                         description: "#{NAME} version"
 end
 
 Mappings do
 end
 
+Conditions do
+end
+
+Transform do
+end
+
 Resources do
-  _s3_bucket "#{NAME}"
+  # And more kumogata-template examples at examples/
+
+  # Create a S3 Bucket
+  #_s3_bucket "#{NAME}"
 end
 
 Outputs do
-  _output_s3 "#{NAME}"
+  # Output S3 Bucket Information
+  #_output_s3 "#{NAME}"
 end
